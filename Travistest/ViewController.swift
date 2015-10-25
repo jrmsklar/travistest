955a1a7960082778d470a05c8159e3bb20d9b0df
Josh Sklar <josh@Joshs-MacBook.local>
2015-10-25 02:29:09 -0400

    initial commit

--- Travistest/ViewController.swift
+++ Travistest/ViewController.swift
@@ -19,7 +19,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+enum MyEnum {
+    case First
+    case Second
+}
+
+extension MyEnum : CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .First:
+            return "This is the first case"
+        case .Second:
+            return "This is the second case"
+        }
+    }
+}
---
+++ TravistestTests/ViewControllerTests.swift
@@ -0,0 +1,20 @@
+//
+//  ViewControllerTests.swift
+//  Travistest
+//
+//  Created by Josh Sklar on 10/25/15.
+//  Copyright © 2015 Josh Sklar. All rights reserved.
+//
+
+import XCTest
+
+@testable import Travistest
+
+class ViewControllerTests: XCTestCase {
+    
+    func testMyEnumDescription() {
+        XCTAssertEqual(MyEnum.First.description, "This is the first case")
+        XCTAssertEqual(MyEnum.Second.description, "This is the second case")
+    }
+    
+}
