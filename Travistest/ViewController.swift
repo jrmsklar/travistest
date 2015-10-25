282f224c646b31ce0d9eda1de0323e77af9d5ba4
Josh Sklar <jrmsklar@gmail.com>
2015-10-25 03:13:42 -0400

    remove unused function

--- Travistest/ViewController.swift
+++ Travistest/ViewController.swift
@@ -10,11 +10,6 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 }
 
 enum MyEnum {
